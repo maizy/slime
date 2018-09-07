cf9110fc0733212bb1060626fc682d4f1555edeb
Nikita Kovaliov <nikita@maizy.ru>
2018-09-07 19:18:40 +0300

    iss #1: input spec

--- slime/src/test/scala/space/maizy/slime/InputSpec.scala
+++ slime/src/test/scala/space/maizy/slime/InputSpec.scala
@@ -5,12 +5,30 @@ package space.maizy.slime
  * See LICENSE.txt for details.
  */
 
+import cats.data.NonEmptyList
+
 class InputSpec extends BaseSpec {
   "Input" should "returns selected arg" in {
-    fail("todo")
+    val input = Input(
+      NonEmptyList.of(
+        InputArg("arg"),
+        InputDivider(" "),
+        InputArg("arg2", Some(4))
+      )
+    )
+    input.selectedArg shouldBe Some(InputArg("arg2", Some(4)))
+    input.selected shouldBe Some(InputArg("arg2", Some(4)))
   }
 
   it should "returns selected div" in {
-    fail("todo")
+    val input = Input(
+      NonEmptyList.of(
+        InputArg("arg"),
+        InputDivider(" ", Some(1)),
+        InputArg("arg2")
+      )
+    )
+    input.selectedDiv shouldBe Some(InputDivider(" ", Some(1)))
+    input.selected shouldBe Some(InputDivider(" ", Some(1)))
   }
 }
